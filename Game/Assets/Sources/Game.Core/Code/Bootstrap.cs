ea6a54ad15fd623221315d59da72ad0032bd6bff
Xavier Arpa <arpaxavier@gmail.com>
2022-10-20 01:48:07 +0200

    - Avances.

---
+++ Game/Assets/Sources/Game.Core/Code/Bootstrap.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bootstrap : MonoBehaviour
+{
+    private IEnumerator Start()
+    {
+        yield return null;
+    }
+
+}
