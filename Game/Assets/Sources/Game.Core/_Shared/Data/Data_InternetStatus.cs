2cc79dc6939716bf920ea0913c14115ebd7b3f83
Xavier Arpa <arpaxavier@gmail.com>
2022-10-20 11:11:50 +0200

    - Avances.

--- Game/Assets/Sources/Game.Core/_Shared/Data/Data_InternetStatus.cs
+++ Game/Assets/Sources/Game.Core/_Shared/Data/Data_InternetStatus.cs
@@ -8,12 +8,4 @@ public static class Data_InternetStatus
 {
     public const string URL_FIREBASE = "firebase.google.com";
     public const string URL_GOOGLE = "www.google.com";
-    public const string URL_FIREBASE_IDLE_RTDB = "https://idle-hospital-83612985-default-rtdb.europe-west1.firebasedatabase.app";
-    //public const string URL_FIREBASE_IDLE = "idle-hospital-83612985.appspot.com";
-
 }
-
-/*
- * 
- --- PING idle-hospital-83612985.appspot.com (142.250.74.116) 56(84) bytes of data. ---
- */
